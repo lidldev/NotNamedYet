b1e987a5d482249b9bc2fe520bd9111095b36a5f
Ahmet Affan <ahmet.affan.ebcioglu@outlook.com>
2024-04-23 22:31:48 +0300

    assets upgrade

--- assets/assets.go
+++ assets/assets.go
@@ -1,1 +1,67 @@
 package assets
+
+import (
+	"embed"
+	"fmt"
+	"image"
+	"log"
+	"os"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/lafriks/go-tiled"
+	"github.com/lafriks/go-tiled/render"
+)
+
+type Assets struct{}
+
+//go:embed *
+var assets embed.FS
+
+func getSingleImage(name string) *ebiten.Image {
+	f, err := assets.Open(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return ebiten.NewImageFromImage(img)
+}
+
+func getTiled(name string) *ebiten.Image {
+	gameMap, err := tiled.LoadFile(name)
+	if err != nil {
+		fmt.Printf("error parsing map: %s", err.Error())
+		os.Exit(2)
+	}
+
+	fmt.Println(gameMap)
+
+	renderer, err := render.NewRenderer(gameMap)
+	if err != nil {
+		fmt.Printf("map unsupported for rendering: %s", err.Error())
+		os.Exit(2)
+	}
+
+	// Render just layer 0 to the Renderer.
+	err = renderer.RenderLayer(0)
+	if err != nil {
+		fmt.Printf("layer unsupported for rendering: %s", err.Error())
+		os.Exit(2)
+	}
+
+	err = renderer.RenderLayer(1)
+	if err != nil {
+		fmt.Printf("layer unsupported for rendering: %s", err.Error())
+		os.Exit(2)
+	}
+
+	img := renderer.Result
+
+	renderer.Clear()
+
+	return ebiten.NewImageFromImage(img)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/lidldev/NotNamedYet
 
 go 1.22.0
+
+require (
+	github.com/hajimehoshi/ebiten/v2 v2.7.2
+	github.com/lafriks/go-tiled v0.12.0
+)
+
+require (
+	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/ebitengine/gomobile v0.0.0-20240329170434-1771503ff0a8 // indirect
+	github.com/ebitengine/hideconsole v1.0.0 // indirect
+	github.com/ebitengine/purego v0.7.0 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
+	golang.org/x/image v0.15.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
