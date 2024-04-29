00ab6e2cf14ba254d83d7f00b41fec8083894907
Ahmet Affan <ahmet.affan.ebcioglu@outlook.com>
2024-04-29 23:11:47 +0300

    walls?

--- game/game.go
+++ game/game.go
@@ -2,6 +2,7 @@ package game
 
 import (
 	"fmt"
+	"image/color"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -21,10 +22,37 @@ func NewGame() *Game {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(1, 1.422)
+	rect := ebiten.NewImage(16, 720)
+	rect.Fill(color.White)
+
+	op2 := &ebiten.DrawImageOptions{}
+	op2.GeoM.Scale(1.422, 1)
+	rect2 := ebiten.NewImage(720, 16)
+	rect2.Fill(color.White)
+
+	op3 := &ebiten.DrawImageOptions{}
+	op3.GeoM.Scale(16, 1.422)
+	op3.GeoM.Translate(1008, 0)
+	rect3 := ebiten.NewImage(1, 720)
+	rect3.Fill(color.White)
+
+	op4 := &ebiten.DrawImageOptions{}
+	op4.GeoM.Scale(1.422, 1)
+	op4.GeoM.Translate(0, 1008)
+	rect4 := ebiten.NewImage(720, 16)
+	rect4.Fill(color.White)
+
 	g.camera.clear()
 	g.camera.draw(assets.Tile, &ebiten.DrawImageOptions{})
 
 	g.player.Draw(screen, &g.camera, g)
+
+	g.camera.draw(rect, op)
+	g.camera.draw(rect2, op2)
+	g.camera.draw(rect3, op3)
+	g.camera.draw(rect4, op4)
 
 	g.camera.render(screen)
 
@@ -39,15 +67,15 @@ func (g *Game) Update() error {
 	g.player.Update()
 	g.camera.setPos(g.player.player.x/unit-320, g.player.player.y/unit-240)
 
-	if g.player.player.x <= 3150 && g.player.player.y <= 2350 {
-		g.camera.setPos(0, g.player.player.y/unit-240)
-		g.camera.setPos(0, 0)
-	}
+	// if g.player.player.x <= 3150 && g.player.player.y <= 2350 {
+	// 	g.camera.setPos(0, g.player.player.y/unit-240)
+	// 	g.camera.setPos(0, 0)
+	// }
 
-	if g.player.player.x >= 7040 && g.player.player.y >= 7845 {
-		g.camera.setPos(385, g.player.player.y/unit-240)
-		g.camera.setPos(g.player.player.x/unit, 240)
-	}
+	// if g.player.player.x >= 7040 && g.player.player.y >= 7845 {
+	// 	g.camera.setPos(385, g.player.player.y/unit-240)
+	// 	g.camera.setPos(g.player.player.x/unit, 240)
+	// }
 
 	vsync := ebiten.IsVsyncEnabled()
 
